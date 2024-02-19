36e75bff69aab24095179a3dcdc1c9c47598395d
GargDivanshu <divanshu.garg113@gmail.com>
2024-02-19 18:21:11 +0530

    sol check

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import App from "./components/App";
+import store from "./components/store";
 
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
